a6e381ac99a4c470e0a67429993a8fd17dbc3888
Nguyễn Phương Điền <diennguyenphuong2003@gmail.com>
2024-10-13 16:22:07 +0700

    Cập nhật booking

--- Hotel.Application/Interfaces/IBookingService.cs
+++ Hotel.Application/Interfaces/IBookingService.cs
@@ -9,7 +9,7 @@ namespace Hotel.Application.Interfaces
     public interface IBookingService
     {
       
-        Task<PaginatedList<GetBookingDTO>> GetPageAsync(int index, int pageSize, string idSearch, string customerID,string employeeID);
+        Task<PaginatedList<GetBookingDTO>> GetPageAsync(int index, int pageSize, string idSearch, string customerID,string employeeID, DateOnly? bookingDate, DateOnly ?  checkInDate);
         Task<GetBookingDTO> CreateBooking(PostBookingDTO model);
         Task UpdateStatusBooking(string bookingID, EnumBooking enumBooking);
         Task<List<GetBookingDTO>> GetBookingByCustomerId(string CustomerId, EnumBooking enumBooking);
