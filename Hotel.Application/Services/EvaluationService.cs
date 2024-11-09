7c2201098f24d01df6a8f0a9f1ebe6a0ccc4ae54
TranNgocThanh07 <thanhtran.070103@gmail.com>
2024-11-10 00:03:22 +0700

    danh gia

--- Hotel.Application/Services/EvaluationService.cs
+++ Hotel.Application/Services/EvaluationService.cs
@@ -153,7 +153,7 @@ namespace Hotel.Application.Services
 
             roomType.AverageStart = (roomType.AverageStart + model.Starts) / 2;
 
-            await _unitOfWork.GetRepository<RoomType>().InsertAsync(roomType);
+            await _unitOfWork.GetRepository<RoomType>().UpdateAsync(roomType);
             await _unitOfWork.GetRepository<Evaluation>().InsertAsync(evaluation);
             await _unitOfWork.SaveChangesAsync();
             //Thêm hình ảnh
